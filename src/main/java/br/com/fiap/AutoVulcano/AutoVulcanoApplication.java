e478c087468d4775e7671d7f08a1f7687b6fad04
vinnybrito <viny.rbrito15@hotmail.com>
2024-03-22 14:43:29 -0300

    Atualizado repositório

--- src/main/java/br/com/fiap/AutoVulcano/AutoVulcanoApplication.java
+++ src/main/java/br/com/fiap/AutoVulcano/AutoVulcanoApplication.java
@@ -2,6 +2,7 @@ package br.com.fiap.AutoVulcano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 
@@ -10,5 +11,6 @@ public class AutoVulcanoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutoVulcanoApplication.class, args);
 	}
+
 
 }
